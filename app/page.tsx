515f04402b87865591f73c05dff27aa0d94755e9
Emmelunga <87323291+matterconi@users.noreply.github.com>
2024-12-11 17:59:47 +0100

    Added eslint config

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Page = () => {
   return (
     <div>
-      <h1 className="">Home</h1>
+      <h1 className="flex gap-4 text-3xl font-black text-white  ">Home</h1>
     </div>
   );
 };
